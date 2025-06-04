7d53d5be51ae101b4752c8fe6ec9119b17fd25a9
Joera <joera@joeramulders.com>
2025-06-04 13:44:33 +0200

    updated main

--- scripts/render.ts
+++ scripts/render.ts
@@ -1,15 +1,21 @@
 import { LitNodeClient } from "@lit-protocol/lit-node-client";
 import { LIT_ABILITY, LIT_NETWORK, LIT_RPC } from "@lit-protocol/constants";
 import * as ethers from "ethers";
-import 'dotenv/config';
+import "dotenv/config";
 import { LitContracts } from "@lit-protocol/contracts-sdk";
-import { LitPKPResource, LitActionResource, createSiweMessageWithRecaps, generateAuthSig } from "@lit-protocol/auth-helpers";
-import { uploadToPinata } from './pinata';
-import { clearFolder} from './fs';
-import { downloadHTML} from './ipfs';
+import {
+  LitPKPResource,
+  LitActionResource,
+  createSiweMessageWithRecaps,
+  generateAuthSig,
+} from "@lit-protocol/auth-helpers";
+import { uploadToPinata } from "./pinata";
+import { clearFolder } from "./fs";
+import { downloadHTML } from "./ipfs";
 
-const MAIN_ACTION_HASH = "QmfWpwdauviZ7FFjyGEVXkjZwWjYdAF6cDVNT2P2q8UUME";
-const STREAM_ID = 'kjzl6kcym7w8y7mamk4c4xdbnn4y963xih6fbe1blmw1ac20ac4rtbakp24psou'
+const MAIN_ACTION_HASH = "Qmdi5ZsK3YVVVDnJiyTKANBAt6kx7TjoKD9rxPDdKCgScx";
+const STREAM_ID =
+  "kjzl6kcym7w8y7mamk4c4xdbnn4y963xih6fbe1blmw1ac20ac4rtbakp24psou";
 const publication = "unamore.--web.eth";
 const safeAddress = "0xd065d8C47994cAC57e49785aCEE04FcA495afac4";
 
@@ -17,128 +23,114 @@ const epk = process.env.PRIVATE_KEY_UNAMORE || process.env.PRIVATE_KEY || "";
 const SELECTED_LIT_NETWORK = LIT_NETWORK.Datil;
 
 const main = async () => {
+  let render_action = await uploadToPinata("./renderer/dist/main.js");
 
-    let render_action = await uploadToPinata('./renderer/dist/main.js');
+  console.log("render CID:", render_action.IpfsHash);
 
-    console.log("render CID:", render_action.IpfsHash);
+  const litNodeClient = new LitNodeClient({
+    litNetwork: SELECTED_LIT_NETWORK,
+    debug: false,
+  });
 
-    const litNodeClient = new LitNodeClient({
-        litNetwork: SELECTED_LIT_NETWORK,
-        debug: false
+  await litNodeClient.connect();
+  const ethersWallet = new ethers.Wallet(
+    epk,
+    new ethers.providers.JsonRpcProvider(LIT_RPC.CHRONICLE_YELLOWSTONE),
+  );
+
+  let capacityTokenId: string = "";
+  if (!process.env.CAPACITY_TOKEN_ID) {
+    const litContracts = new LitContracts({
+      signer: ethersWallet,
+      network: SELECTED_LIT_NETWORK,
+    });
+    await litContracts.connect();
+
+    capacityTokenId = (
+      await litContracts.mintCapacityCreditsNFT({
+        requestsPerKilosecond: 10,
+        daysUntilUTCMidnightExpiration: 1,
+      })
+    ).capacityTokenIdStr;
+
+    console.log("Capacity token ID:", capacityTokenId);
+  } else {
+    capacityTokenId = process.env.CAPACITY_TOKEN_ID!;
+  }
+
+  const { capacityDelegationAuthSig } =
+    await litNodeClient.createCapacityDelegationAuthSig({
+      dAppOwnerWallet: ethersWallet,
+      capacityTokenId,
+      delegateeAddresses: [ethersWallet.address],
+      uses: "1",
     });
 
-    await litNodeClient.connect();
-    const ethersWallet = new ethers.Wallet(
-        epk,
-        new ethers.providers.JsonRpcProvider(LIT_RPC.CHRONICLE_YELLOWSTONE)
-    );
+  const sessionSignatures: any = await litNodeClient.getSessionSigs({
+    chain: "yellowstone",
+    capabilityAuthSigs: [capacityDelegationAuthSig],
+    expiration: new Date(Date.now() + 1000 * 60 * 60 * 24).toISOString(), // 24 hours
+    resourceAbilityRequests: [
+      {
+        resource: new LitPKPResource("*"),
+        ability: LIT_ABILITY.PKPSigning,
+      },
+      {
+        resource: new LitActionResource("*"),
+        ability: LIT_ABILITY.LitActionExecution,
+      },
+    ],
+    authNeededCallback: async ({
+      resourceAbilityRequests,
+      expiration,
+      uri,
+    }) => {
+      const toSign = await createSiweMessageWithRecaps({
+        uri: uri!,
+        expiration: expiration!,
+        resources: resourceAbilityRequests!,
+        walletAddress: ethersWallet.address,
+        nonce: await litNodeClient.getLatestBlockhash(),
+        litNodeClient,
+      });
 
-    let capacityTokenId: string = "";
-    if (!process.env.CAPACITY_TOKEN_ID) {
+      return await generateAuthSig({
+        signer: ethersWallet,
+        toSign,
+      });
+    },
+  });
 
-        const litContracts = new LitContracts({
-            signer: ethersWallet,
-            network: SELECTED_LIT_NETWORK,
-        });
-        await litContracts.connect();
+  // console.log(1);../renderAction/main.js
+  // console.log(MAIN_ACTION_HASH);
+  // console.log(sessionSignatures);
 
-        capacityTokenId = (
-            await litContracts.mintCapacityCreditsNFT({
-            requestsPerKilosecond: 10,
-            daysUntilUTCMidnightExpiration: 1,
-            })
-        ).capacityTokenIdStr;
+  console.log(render_action.IpfsHash);
 
-        console.log("Capacity token ID:", capacityTokenId);
-
-    } else {
-        capacityTokenId = process.env.CAPACITY_TOKEN_ID!;
-    }
-
-    const { capacityDelegationAuthSig } =
-        await litNodeClient.createCapacityDelegationAuthSig({
-        dAppOwnerWallet: ethersWallet,
-        capacityTokenId,
-        delegateeAddresses: [ethersWallet.address],
-        uses: "1",
-        });
-
-    const sessionSignatures: any = await litNodeClient.getSessionSigs({    
-        chain: "yellowstone",
-        capabilityAuthSigs: [capacityDelegationAuthSig],
-        expiration: new Date(Date.now() + 1000 * 60 * 60 * 24).toISOString(), // 24 hours
-        resourceAbilityRequests: [
-            {
-            resource: new LitPKPResource("*"),
-            ability: LIT_ABILITY.PKPSigning,
-            },
-            {
-            resource: new LitActionResource("*"),
-            ability: LIT_ABILITY.LitActionExecution,
-            },
-        ],
-        authNeededCallback: async ({
-            resourceAbilityRequests,
-            expiration,
-            uri,
-        }) => {
-            const toSign = await createSiweMessageWithRecaps({
-            uri: uri!,
-            expiration: expiration!,
-            resources: resourceAbilityRequests!,
-            walletAddress: ethersWallet.address,
-            nonce: await litNodeClient.getLatestBlockhash(),
-            litNodeClient,
-            });
-
-            return await generateAuthSig({
-            signer: ethersWallet,
-            toSign,
-            });
-        },
+  try {
+    const action: any = await litNodeClient.executeJs({
+      sessionSigs: sessionSignatures,
+      ipfsId: MAIN_ACTION_HASH,
+      jsParams: {
+        safeAddress,
+        publication,
+        stream_id: STREAM_ID,
+        publish: false,
+      },
     });
 
-    // console.log(1);../renderAction/main.js
-    // console.log(MAIN_ACTION_HASH);
-    // console.log(sessionSignatures);
+    console.log(action.logs);
 
-    console.log(render_action.IpfsHash);
+    const response = JSON.parse(action.response);
 
-    try {
-
-        const action: any = await litNodeClient.executeJs({
-            sessionSigs: sessionSignatures,
-            ipfsId: MAIN_ACTION_HASH,
-            jsParams: { 
-                safeAddress,
-                publication, 
-                stream_id: STREAM_ID,
-                publish: false
-            } 
-        });
-
-
-        console.log(action.logs);
-
-        const response = JSON.parse(action.response);
-
-
-
-        if(response.rootCid) {
-
-            const folder = './html'
-            clearFolder(folder);
-            await downloadHTML(response.rootCid, folder);
-
-        }
-
-
-    } catch (error) {   
-        console.error("Error deploying action:", error);
+    if (response.rootCid) {
+      const folder = "./html";
+      clearFolder(folder);
+      await downloadHTML(response.rootCid, folder);
     }
-
-
-
-}
+  } catch (error) {
+    console.error("Error deploying action:", error);
+  }
+};
 
 main();
