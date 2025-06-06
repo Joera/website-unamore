e9dc5ae02c8aa5908a4e3ac5cf9f3a83442f8c58
Joera <joera@joeramulders.com>
2025-06-06 10:55:33 +0200

    working on with helper

--- renderer/dist/main.js
+++ renderer/dist/main.js
@@ -1054,7 +1054,6 @@
         console.error("Empty template after cleaning");
         return "";
       }
-      templateData2 = templateData2.filter((d) => d != null);
       console.log(templateData2);
       const partialFiles = templateArray.filter(
         (t) => t.path.includes("partials/")
